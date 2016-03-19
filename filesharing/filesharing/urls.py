f7b2eff12fc0253aee4326ee9c43a16c8d0e6b74
yosufali <yosufali@users.noreply.github.com>
2016-03-19 20:44:08 +0000

    Added File model, index view, index template, fixed urls and added app to settings

--- filesharing/filesharing/urls.py
+++ filesharing/filesharing/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+from share import urls as shareurls
+
 urlpatterns = [
+    url(r'^', include(shareurls)),
     url(r'^admin/', admin.site.urls),
 ]
--- filesharing/share/models.py
+++ filesharing/share/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.utils import timezone
+
 # Create your models here.
+
+
+class File(models.Model):
+
+    name = models.CharField(max_length=500)
+    time_uploaded = models.DateTimeField(default=timezone.now())
+    file = models.FileField(upload_to='files')
