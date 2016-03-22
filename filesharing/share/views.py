711836a47af0b281e025d25370e7fb8a10a7eb51
yosufali <yosufali@users.noreply.github.com>
2016-03-22 10:41:39 +0000

    File uploades with redirection to list

--- filesharing/share/views.py
+++ filesharing/share/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 from django.utils import timezone
 
 from share.models import File
@@ -11,7 +14,6 @@ from share.forms import FileForm
 def index(request):
 
     return render(request, 'index.html', {})
-
 
 # def upload_file(request):
 
@@ -30,20 +32,17 @@ def upload_file(request):
             newdoc.save()
 
             # Redirect to the document list after POST
-            return redirect('index')
+            return HttpResponseRedirect(reverse('upload'))
+            #return redirect('index')
     else:
         form = FileForm()  # A empty, unbound form
 
-    # # Load documents for the list page
-    # file = File.objects.all()
+    # Load documents for the list page
+    files = File.objects.all()
 
-    # # Render list page with the documents and the form
-    # return render_to_response(
-    #     'list.html',
-    #     {'documents': file, 'form': form},
-    #     context_instance=RequestContext(request)
-    # )
-#def handle_uploaded_file(f):
-    #with open('some/file/name.txt', 'wb+') as destination:
-        #for chunk in f.chunks():
-            #destination.write(chunk)
+    # Render list page with the documents and the form
+    return render_to_response(
+        'list.html',
+        {'files': files, 'form': form},
+        context_instance=RequestContext(request)
+    )
